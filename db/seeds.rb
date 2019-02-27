08083b238eb7e0efea9277f7cd9cdd5b95af48a1
Nicolas Desbonnets <nicolas.desbonnets@hec.edu>
2019-02-27 14:46:49 +0100

    restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+10.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_address
+  category = ['italian', 'chinese', 'french', 'belgian', 'japanese'].sample
+  phone = Faker::PhoneNumber.cell_phone
+  restaurant = Restaurant.new(name: name, address: address, category: category, phone_number: phone)
+  restaurant.save
+end
